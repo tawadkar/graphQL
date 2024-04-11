32551822f5224194fff687f4ee00c17adab0102f
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2024-04-11 09:29:47 +0530

    Logic to Create Books at while starting the server

--- src/main/java/com/graphql/poc/GraphqlDemoApplication.java
+++ src/main/java/com/graphql/poc/GraphqlDemoApplication.java
@@ -1,13 +1,37 @@
 package com.graphql.poc;
 
+import com.graphql.poc.entities.Book;
+import com.graphql.poc.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlDemoApplication {
+public class GraphqlDemoApplication implements CommandLineRunner {
 
+	@Autowired
+	private BookService bookService;
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+        Book b1 = new Book();
+		b1.setTitle("GraphQL Basics");
+		b1.setDesc("Learn GrapthQL from Basic");
+		b1.setPrice(4000);
+		b1.setAuthor("TestGraphQL");
+
+		Book b2 = new Book();
+		b2.setTitle("Python Basics");
+		b2.setDesc("Learn Python from Basic");
+		b2.setPrice(5000);
+		b2.setAuthor("TestPyton");
+        
+		//Saves book in Database
+		this.bookService.create(b1);
+		this.bookService.create(b2);
+	}
 }
